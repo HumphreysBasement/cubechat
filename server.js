1ff9c71adbf077e394e02ad5b2f5c8ade6435326
jeffersonparkavenue <jyhs1014@gmail.com>
2021-07-20 21:41:46 -0400

    dasf

--- server.js
+++ server.js
@@ -1,11 +1,33 @@
 const WebSocket = require('ws');
-const PORT =  process.env.PORT;
+const PORT =  process.env.PORT | 8080;
 const wss = new WebSocket.Server({ port: PORT });
 
+//invoked upon a successful connection
 wss.on('connection', function connection(ws) {
+  //invoked when a WebSocket message from a client is received
   ws.on('message', function incoming(message) {
+
+    message = JSON.parse(message);
+
+    if(message.type == "name"){
+      ws.nickname = message.data;
+      return;
+    }
+
     console.log('received: ', message);
+
+    wss.clients.forEach((client) => {
+      if(client != ws)
+      client.send(JSON.stringify({
+        name: ws.nickname,
+        data: message.data
+      }));
+    });
   });
 
-  ws.send('something');
+  ws.on('close', () => {
+    console.log( ws.nickname +" left");
+  });
+
+  console.log(ws.nickname + " joined");
 });
