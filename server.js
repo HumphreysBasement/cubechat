44e8c27b3b80f7089d11db6e87a2f9d41d42eb13
jeffersonparkavenue <jyhs1014@gmail.com>
2021-07-22 16:56:10 -0400

    adf

--- server.js
+++ server.js
@@ -2,106 +2,140 @@ const WebSocket = require('ws');
 const PORT =  process.env.PORT || 8080;
 const wss = new WebSocket.Server({ port: PORT });
 
-/*
-1. establish connexion
-client connect to server at ws://{host}/chat/{username}
-
-so, server now can associate the username with the client.
-
-server reject client if username is taken
-
-send those things in json to client nested
-*/
-
-//                                              space
-
-//invoked upon a successful connection
 // req is type http.IncomingMessage: https://nodejs.org/api/http.html#http_class_http_incomingmessage
+
+// send a json to everyone
+const broadcast = json => {
+    for (const client of wss.clients)
+        client.send(JSON.stringify(json))
+}
+
 wss.on('connection', function connection(ws, req) {
-   //invoked when a WebSocket message from a client is received
+    //invoked when a WebSocket message from a client is received
     const parts = req.url.split('/')
     ws.nickname = parts[parts.length-1]
 
     if ([...wss.clients].some(w => w !== ws && w.nickname === ws.nickname)) {
-        ws.send(JSON.stringify({ type: 'reject', message: 'jim ryan' }))
+        ws.send(JSON.stringify({ type: 'reject', status: 'zim ryan' }))
         ws.close(1000)
         return;
     }
 
-    ws.x = ws.y = ws.z = 0;
+/*
+
+to do 'movement', just send a lot of small movement to client
+
+when client says 'move', do not teleport it instantly. instead, store as like
+    'target destination'
+    and then every 50 ms or something, move all cubes
+        a tiny bit closer towards their 'target destination'
+          (a specific amount of lenght)
+
+*/
 
 
-    //show new user to already users
-    wss.clients.forEach((client) => {
-        client.send(JSON.stringify({
-            type: 'existance',
-            username: ws.nickname,
-            x: ws.x,
-            y: ws.y,
-            z: ws.z
-        }));
-    });
+    ws.currPosition   = { x: 0, y: 0, z: 0 }
+    ws.targetPosition = { x: 0, y: 0, z: 0 }
 
-    //show already user to new user
-    wss.clients.forEach((client) => {
-        if (client === ws) return;
+    //show new user to already existing users
+    broadcast({
+        type: 'existance',
+        username: ws.nickname,
+        ...ws.currPosition
+    })
+
+    //show already exsiting users to new user
+    for (const client of wss.clients) {
+        if (client === ws) continue;
         ws.send(JSON.stringify({
             type: 'existance',
             username: client.nickname,
-            x: client.x,
-            y: client.y,
-            z: client.z
+            ...client.currPosition
         }));
-    });
+    }
 
     ws.on('message', function incoming(message) {
         message = JSON.parse(message);
-
         console.log('received: ', message);
-        //existence
-        //your code goes here:
-
-        //move
-        //your code goes here:
-        if(message.type == "move"){
-            ws.x = message.x;
-            ws.y = message.y;
-            ws.z = message.z;
-            wss.clients.forEach((client) => {
-                client.send(JSON.stringify({
-                    type: 'move',
-                    username: ws.nickname,
-                    x: ws.x,
-                    y: ws.y,
-                    z: ws.z
-                }));
-            });
+        if(message.type === "move"){
+            ws.targetPosition = {
+                x: message.x,
+                y: message.y,
+                z: message.z
+            }/*
+            broadcast({
+                type: 'move',
+                username: ws.nickname,
+                x: ws.x,
+                y: ws.y,
+                z: ws.z
+            })*/
             return;
-        }
-
-        //say
-        //your code goes here:
-        if(message.type == "say"){
-            wss.clients.forEach((client) => {
-                client.send(JSON.stringify({
-                    type: 'say',
-                    username: ws.nickname,
-                    message: message.message
-                }));
-            });
+        } else if (message.type === "say") {
+            broadcast({ // jim ryan
+                type: 'say',
+                username: ws.nickname,
+                message: message.message
+            })
             return;
         }
     });
+    
     ws.on('close', () => {
+        console.log(ws.nickname + " joined");
         //nonexistence
-        //your code goes here:
-        wss.clients.forEach((client) => {
-            client.send(JSON.stringify({
-            type: 'nonexistance',
-            username: ws.nickname
-            }));
-        })
-    });
-
-  console.log(ws.nickname + " joined");
+        broadcast({ type: 'nonexistance', username: ws.nickname })
+    })
 });
+
+/*
+
+j i m
+i   i
+m i j
+
+*/
+
+// 'normalised' vector times movespeed!!! 'asam'
+const tickspeed = 0.1  // send updates every {tickspeed} milliseconds
+const movespeed = 0.002 // move 0.8 units per update
+
+// what other functions would we need to calculate a 'noramlised' vector?
+
+// totally n
+const v3sub = (v1, v2) => ({ x: v2.x - v1.x, y: v2.y - v1.y, z: v2.z - v1.z })
+
+const v3add = (v1, v2) => ({ x: v2.x + v1.x, y: v2.y + v1.y, z: v2.z + v1.z })
+
+const v3dot = (v1, v2) => v1.x*v2.x + v1.y*v2.y + v1.z*v2.z
+
+const v3mag = v => Math.sqrt(v3dot(v, v))
+
+const v3mul_by_scalar = (v, s) => ({ x: v.x*s, y: v.y*s, z: v.z*s })
+
+const v3div_by_scalar = (v, s) => ({ x: v.x/s, y: v.y/s, z: v.z/s })
+
+const v3norm = v => v3div_by_scalar(v, v3mag(v))
+
+// every ?? ms move closer to target destination asam
+const tinymove = (cube) => {
+    let vec = v3sub(cube.currPosition, cube.targetPosition);
+    const mag = v3mag(vec)
+    if (mag === 0) {
+        return
+    }
+    // does not let cube go farther than intended
+    if (mag < movespeed) { // manually teleport
+        cube.currPosition = { ...cube.targetPosition }
+    } else {
+        let dir = v3norm(vec);
+        cube.currPosition.x += movespeed*(dir.x);
+        cube.currPosition.y += movespeed*(dir.y);
+        cube.currPosition.z += movespeed*(dir.z);
+    }
+    broadcast({ type: 'move', username: cube.nickname, ...cube.currPosition })
+}
+
+// this runs  '_ => wss.clients.forEach(tinymove)' every, tickspeed milliseconds
+// big controller
+const move = setInterval(_ => wss.clients.forEach(tinymove), tickspeed);
